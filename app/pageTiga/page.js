e5a5d0d2f454b587303b170b76452ceb177ec6f5
ZefanyaPrasetiyo <zefanyaprasetiyo29>
2025-07-29 11:35:21 +0700

    commit kedua

--- app/pageTiga/page.js
+++ app/pageTiga/page.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function pageTiga () {
     return (
         <div>
-            <h1>Selamat datang di halaman ke 3</h1>
+            <h1>Selamat datang di halaman ke 3 ini halaman terakhir</h1>
             <nav>
                 <p>halaman <Link href={"/pageDua"}>sebelumnya...</Link></p> 
             </nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+$a = 1;
+$b = 2;
+
+print($a + $b * $b)
+
+echo "halo aku jepan";
+?>
